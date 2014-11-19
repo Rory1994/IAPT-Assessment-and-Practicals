14e6426728b0ed0f690050309fa721da702149b2
Rory1994 <rs1019@york.ac.uk>
2014-11-19 22:22:46 +0000

    Created Profile Page

--- BootUp/controllers/profile.py
+++ BootUp/controllers/profile.py
@@ -1,3 +1,11 @@
 
+@auth.requires_login(otherwise=URL('default','login'))
 def profile():
-    return dict()
+
+    user = (db(db.auth_user.id == auth._get_user_id()).select())[0]
+
+    number_of_projects = len(db(db.project.username == auth._get_user_id()).select())
+    number_of_pledges = len(db(db.pledges.username == auth._get_user_id()).select())
+
+
+    return dict(user=user, number_of_projects=number_of_projects, number_of_pledges=number_of_pledges )
